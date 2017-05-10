eca350b8354d29ca7c650dff65d92f1fed8450db
Mitch Thompson <thomitchell@gmail.com>
2017-05-10 16:53:44 -0700

    Adds HW5 fix to layout error

---
+++ AD340project/app/src/main/java/assignments/mitch/ad340project/BackgroundTask.java
@@ -0,0 +1,58 @@
+package assignments.mitch.ad340project;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class BackgroundTask {
+    private static final String TAG = BackgroundTask.class.getSimpleName();
+    Context context;
+    String jsonURL = "http://mitchlthompson.com/ad340/movieslist.json";
+    ArrayList<MovieObject> arrayList = new ArrayList<>();
+
+    public BackgroundTask(Context ctx){
+
+        this.context = ctx;
+    }
+
+    public ArrayList<MovieObject> getList(){
+
+        final JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, jsonURL, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int count=0;count<response.length();count++)
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(count);
+                                MovieObject movie = new MovieObject(jsonObject.getString("title"), jsonObject.getString("year"));
+                                arrayList.add(movie);
+                                //Log.e(TAG, "Test: " + jsonObject.getString("title") + " " + jsonObject.getString("year"));
+
+                            } catch (JSONException e) {
+
+                                Log.e(TAG, "JSONException: " + e.toString());
+                            }
+
+                        MovieListActivity.recyclerViewAdapter.notifyDataSetChanged();
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Volley Error: " + error.toString());
+            }
+        });
+        MySingleton.getSingleton(context).addToRequest(jsonArrayRequest);
+        return arrayList;
+    };
+}
---
+++ AD340project/app/src/main/java/assignments/mitch/ad340project/MovieObject.java
@@ -0,0 +1,30 @@
+package assignments.mitch.ad340project;
+
+
+public class MovieObject {
+    String title;
+    String year;
+
+    public MovieObject(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
+
+
